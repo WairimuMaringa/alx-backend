8befe39139adcb37cab381330af1d67fab62f76e
WairimuMaringa <makena96wairimu@gmail.com>
2023-04-18 06:48:59 -0700

    task zero

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Class that inherits from basecaching and is
+a caching system
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ Inherits and stores info. """
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        if not key or not self.cache_data.get(key):
+            return None
+        return self.cache_data.get(key)
---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+"""
+class that inherits from basecaching and is
+a caching system
+"""
