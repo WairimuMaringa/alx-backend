ba31bc21ce119449fcac7e2356a814ac9a7cd4a6
WairimuMaringa <makena96wairimu@gmail.com>
2023-04-18 07:25:37 -0700

    task two

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Class that inherits from basecaching and is
+a caching system
+"""
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """ inherits and stores. """
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        if key and item:
+            self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            self.cache_data.pop(self.last_key)
+            print('DISCARD:', self.last_key)
+        if key:
+            self.last_key = key
+
+    def get(self, key):
+        return self.cache_data.get(key)
