bd4f168bcb74e3f97cf759d317c73596a370fe9c
WairimuMaringa <makena96wairimu@gmail.com>
2023-04-18 07:04:20 -0700

    task one

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -3,3 +3,21 @@
 class that inherits from basecaching and is
 a caching system
 """
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ inherits and stores. """
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        if key and item:
+            self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            drop_key = sorted(self.cache_data)[0]
+            self.cache_data.pop(drop_key)
+            print('DISCARD: {}'.format(drop_key))
+
+    def get(self, key):
+        return self.cache_data.get(key)
