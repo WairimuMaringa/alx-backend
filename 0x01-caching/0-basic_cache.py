edb9e8f130dd09c13334ed29493dc0c6c28eb522
WairimuMaringa <makena96wairimu@gmail.com>
2023-04-18 10:26:49 -0700

    task zeroo

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -3,19 +3,23 @@
 Class that inherits from basecaching and is
 a caching system
 """
-BaseCaching = __import__('base_caching').BaseCaching
+from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
     """ Inherits and stores info. """
+
     def __init__(self):
+        """ initialise an instance."""
         super().__init__()
 
     def put(self, key, item):
+        """ add item to cache. """
         if key and item:
             self.cache_data[key] = item
 
     def get(self, key):
+        """ obtain item from cache. """
         if not key or not self.cache_data.get(key):
             return None
         return self.cache_data.get(key)
