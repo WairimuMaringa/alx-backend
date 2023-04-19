abd17f7bb8f40102b1de2a28e1167d118dc37090
WairimuMaringa <makena96wairimu@gmail.com>
2023-04-19 07:13:20 -0700

    task three

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,1 +1,31 @@
 #!/usr/bin/env python3
+"""
+class that inherits from basecaching and
+is a caching system
+"""
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class LRUCache(BaseCaching):
+    """ inherits and stores. """
+
+    def __init__(self):
+        """ initialise instance. """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """ add item to cache. """
+        if key and item:
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key)
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                discarded = self.cache_data.popitem(last=False)
+                print('DISCARD: {}'.format(discarded[0]))
+
+    def get(self, key):
+        """ obtain item from cache. """
+        if key in self.cache_data:
+            self.cache_data.move_to_end(key)
+            return self.cache_data[key]
